202696b414ffb9180dc98dfccd5007f55605b6c1
User <User@JulioSousa-XPS.Home>
2023-04-16 22:00:48 +0100

    Atualização código

--- project/src/main/java/GrupoF/project/App.java
+++ project/src/main/java/GrupoF/project/App.java
@@ -1,31 +1,36 @@
 package GrupoF.project;
 
-import java.io.File;
-import org.apache.commons.io.FileUtils;
-import org.json.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.LinkedHashMap;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args ) throws JSONException 
-    {
-      //  System.out.println( "Hello World!" );
-    
-    	String jsonArrayString = "{\fileName\": [{\\\"first name\\\": \\\"GrupoF\\\",\\\"location\\\": \\\"gitHub\\\"}]}"; 
-    	JSONObject output;
-    	try {
-    		JSONArray docs = output.getJSONArray("filename");
-    		File file = new File("File.csv");
-    		String csv = CDL.toString(docs);
-    		FileUtils.writeStringToFile(file, csv);
-    		System.out.println(csv);
-    	}
-    	catch(Exception e) {
-    		e.printStackTrace();
-    	}
-    	
-    }
-}
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        // Define o nome do arquivo CSV de entrada e do arquivo JSON de saída
+        String csvFile = "C:\\Users\\User\\Downloads\\horario_exemplo.csv";
+        String jsonFile = "output.json";
+
+        BufferedReader br = null;
+        String line = "";
+        String csvSplitBy = ";";
+
+        // Cria um objeto JSON para armazenar os dados do CSV
+        JSONObject jsonObject = new JSONObject();
+
+        // Cria um array JSON para armazenar as linhas do CSV
+        JSONArray jsonArray = new JSONArray();
+
+        try {
+
+            // Lê o arquivo CSV e adiciona cada linha ao array JSON
+            br = new BufferedReader(new FileReader(csvFile));
+
+            // Ignora a primeira linha do arquivo CSV
+            br.readLine();
